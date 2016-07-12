b5a6e4e521a675825bdecde6c5c4496e91c25f12
kelaocai <kelaocai@163.com>
2016-07-13 02:53:33 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -562,7 +562,27 @@ class IndexController extends Controller
     
     public function wxjs(){
 
-        $this->assign('js_sign', $this->get_js_sign());
+        $options = [
+            'token' => C('WX_TOKEN'), //填写你设定的key
+            'encodingaeskey' => C('WX_ENCODINGAESKEY'), //填写加密用的EncodingAESKey
+            'appid' => C('WX_APPID'), //填写高级调用功能的app id
+            'appsecret' => C('WX_APPSECRET') //填写高级调用功能的密钥
+        ];
+
+        $weObj = new \Org\Wx\Wechat($options);
+        $auth = $weObj->checkAuth();
+        $js_ticket = $weObj->getJsTicket();
+
+        if (!$js_ticket) {
+            echo "获取js_ticket失败！<br>";
+            echo '错误码：' . $weObj->errCode;
+            echo ' 错误原因：' . ErrCode::getErrText($weObj->errCode);
+            exit;
+        } else {
+        }
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        $js_sign = $weObj->getJsSign($url);
+        $this->assign('js_sign', $js_sign);
         $this->display();
         
     }
@@ -682,6 +702,35 @@ class IndexController extends Controller
         $data['time']=date('y-m-d h:i:s',time());
         $bj->add($data);
 //        echo time();
+    }
+
+
+    public function wx_yao(){
+        $options = [
+            'token' => C('WX_TOKEN'), //填写你设定的key
+            'encodingaeskey' => C('WX_ENCODINGAESKEY'), //填写加密用的EncodingAESKey
+            'appid' => C('WX_APPID'), //填写高级调用功能的app id
+            'appsecret' => C('WX_APPSECRET') //填写高级调用功能的密钥
+        ];
+
+        $weObj = new \Org\Wx\Wechat($options);
+        $auth = $weObj->checkAuth();
+        $js_ticket = $weObj->getJsTicket();
+
+        if (!$js_ticket) {
+            echo "获取js_ticket失败！<br>";
+            echo '错误码：' . $weObj->errCode;
+            echo ' 错误原因：' . ErrCode::getErrText($weObj->errCode);
+            exit;
+        } else {
+//            echo '获取js_ticket成功'.$js_ticket;
+        }
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        $js_sign = $weObj->getJsSign($url);
+        $this->assign('js_sign', $js_sign);
+        $this->assign('js_url', $url);
+        $this->assign('js_ticket', $js_ticket);
+        $this->display();
     }
    
 
